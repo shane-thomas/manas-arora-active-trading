74a1482a5ae0c23d33b9382c8a3f40110e43b972
shane <shaneshaji10@gmail.com>
2024-02-08 23:24:05 +0530

    chore: update .gitignore file

---
+++ src/constants.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timedelta
+
+URL = "https://archives.nseindia.com/content/historical/EQUITIES/"
+DATE_FORMAT = '%Y-%m-%d'
+RESULTS_FILE = 'Results.xlsx'
+ONE_MONTH_FILE = '1 MONTH.xlsx'
+FIVE_DAYS_FILE = '5 DAYS.xlsx'
+THREE_MONTHS_FILE = '3 MONTHS.xlsx'
+DATA_PATH = "DATA"
+ONE_MONTH_PATH = "DATA/1 MONTH"
+FIVE_DAYS_PATH = "DATA/5 DAYS"
+THREE_MONTHS_PATH = "DATA/3 MONTHS"
